51549f4c4a2a16b4186ecfc05173d08e18bb5b1e
tburakonat <tburakonat@gmail.com>
2024-06-22 15:02:38 +0200

    style admin table

--- frontend/src/pages/admin-dashboard.jsx
+++ frontend/src/pages/admin-dashboard.jsx
@@ -6,6 +6,17 @@ import { API_BASE_URL } from "../config";
 const AdminDashboard = () => {
 	const { user } = useAuth();
 	const [users, setUsers] = useState([]);
+	const [filter, setFilter] = useState("");
+	const [roleFilter, setRoleFilter] = useState("all");
+
+	let filteredUsers = users
+		.filter(
+			u =>
+				u.username.toLowerCase().includes(filter.toLowerCase()) ||
+				u.email.toLowerCase().includes(filter.toLowerCase())
+		)
+		.filter(u => roleFilter === "all" || u.role === roleFilter);
+
 	const [error, setError] = useState("");
 	const navigate = useNavigate();
 
@@ -69,47 +80,94 @@ const AdminDashboard = () => {
 	};
 
 	return (
-		<div className="container mx-auto p-4">
-			<h1 className="text-2xl font-bold mb-6 text-center">
-				Admin Dashboard
-			</h1>
-			{error && <p className="text-red-500">{error}</p>}
-			<table className="min-w-full bg-white">
-				<thead>
-					<tr>
-						<th className="py-2 px-4 border-b">Username</th>
-						<th className="py-2 px-4 border-b">Email</th>
-						<th className="py-2 px-4 border-b">Role</th>
-						<th className="py-2 px-4 border-b">Actions</th>
-					</tr>
-				</thead>
-				<tbody>
-					{users.map(u => (
-						<tr key={u._id}>
-							<td className="py-2 px-4 border-b">{u.username}</td>
-							<td className="py-2 px-4 border-b">{u.email}</td>
-							<td className="py-2 px-4 border-b">{u.role}</td>
-							<td className="py-2 px-4 border-b">
-								<select
-									value={u.role}
-									onChange={e =>
-										handleRoleChange(
-											u.username,
-											e.target.value
-										)
-									}
-									className="p-2 border border-gray-300 rounded"
-									disabled={u.username === user.username}
-								>
-									<option value="student">Student</option>
-									<option value="teacher">Teacher</option>
-									<option value="admin">Admin</option>
-								</select>
+		<div
+			className="container mx-auto mb-auto p-4"
+			style={{ maxHeight: "100%" }}
+		>
+			<header className="sticky top-3 bg-gray-100 z-10">
+				<h1 className="text-2xl font-bold mb-6 text-center">
+					Admin Dashboard
+				</h1>
+				{error && <p className="text-red-500">{error}</p>}
+				<div className="mb-4">
+					<label className="block text-gray-700 mb-2" htmlFor="users">
+						Filter Users
+					</label>
+					<input
+						type="text"
+						id="users"
+						name="users"
+						value={filter}
+						onChange={e => setFilter(e.target.value)}
+						className="w-full p-2 border border-gray-300 rounded"
+					/>
+				</div>
+				<div className="mb-4">
+					<label htmlFor="roleFilter" className="mr-2">
+						Filter by role
+					</label>
+					<select
+						id="roleFilter"
+						value={roleFilter}
+						onChange={e => setRoleFilter(e.target.value)}
+						className="p-2 border border-gray-300 rounded"
+					>
+						<option value="all">All</option>
+						<option value="admin">Admin</option>
+						<option value="teacher">Teacher</option>
+						<option value="student">Student</option>
+					</select>
+				</div>
+			</header>
+			<div className="overflow-x-auto max-h-96">
+				<table className="min-w-full bg-white">
+					<thead className="bg-gray-200 sticky top-0 z-10">
+						<tr>
+							<th className="py-2 px-4 border-b">Username</th>
+							<th className="py-2 px-4 border-b">Email</th>
+							<th className="py-2 px-4 border-b">Role</th>
+							<th className="py-2 px-4 border-b">Actions</th>
+						</tr>
+					</thead>
+					<tbody>
+						{filteredUsers.map(u => (
+							<tr key={u._id}>
+								<td className="py-2 px-4 border-b">
+									{u.username}
+								</td>
+								<td className="py-2 px-4 border-b">
+									{u.email}
+								</td>
+								<td className="py-2 px-4 border-b">{u.role}</td>
+								<td className="py-2 px-4 border-b">
+									<select
+										value={u.role}
+										onChange={e =>
+											handleRoleChange(
+												u.username,
+												e.target.value
+											)
+										}
+										className="p-2 border border-gray-300 rounded"
+										disabled={u.username === user.username}
+									>
+										<option value="student">Student</option>
+										<option value="teacher">Teacher</option>
+										<option value="admin">Admin</option>
+									</select>
+								</td>
+							</tr>
+						))}
+					</tbody>
+					<tfoot className="bg-gray-100 sticky bottom-0 z-10">
+						<tr>
+							<td className="py-2 px-4 border-t" colSpan="4">
+								{filteredUsers.length} results found
 							</td>
 						</tr>
-					))}
-				</tbody>
-			</table>
+					</tfoot>
+				</table>
+			</div>
 		</div>
 	);
 };
