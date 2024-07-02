ad7f4d35d5cc7d165ed6bdedeb0e7686cdf8b06d
larissa <larissa.feng@brilliantmade.com>
2024-07-01 22:09:22 -0700

    Add index.js to models folder for easier imports of models

---
+++ backend/models/index.js
@@ -0,0 +1,6 @@
+export { default as Answer } from "./answer";
+export { default as Course } from "./course";
+export { default as Lecture } from "./lecture";
+export { default as LectureQuestion } from "./lectureQuestion";
+export { default as Student } from "./student";
+export { default as User } from "./user";
