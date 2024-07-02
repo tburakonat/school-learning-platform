c95d8b5037b3feb55a3e4667c7aab35fda2030d8
tburakonat <tburakonat@gmail.com>
2024-07-02 10:57:19 +0200

    design teacher dashboard

--- frontend/src/pages/teacher-dashboard.jsx
+++ frontend/src/pages/teacher-dashboard.jsx
@@ -5,7 +5,13 @@ import { API_BASE_URL } from "../config";
 const TeacherDashboard = () => {
 	const { user } = useContext(UserContext);
 	const [students, setStudents] = useState([]);
+	const [filter, setFilter] = useState("");
 	const [error, setError] = useState("");
+	let filteredStudents = students.filter(
+		u =>
+			u.username.toLowerCase().includes(filter.toLowerCase()) ||
+			u.email.toLowerCase().includes(filter.toLowerCase())
+	);
 
 	useEffect(() => {
 		const fetchStudents = async () => {
@@ -32,23 +38,64 @@ const TeacherDashboard = () => {
 	}, []);
 
 	return (
-		<div className="container mx-auto p-4">
-			<h1 className="text-2xl font-bold mb-6 text-center">
-				Teacher Dashboard
-			</h1>
-			{error && <p className="text-red-500">{error}</p>}
-			<div className="bg-white p-8 rounded shadow-md">
-				<h2 className="text-xl font-bold mb-4">
-					Hello, {user.username}
-				</h2>
-				<h3 className="text-lg font-bold mb-2">Students List</h3>
-				<ul className="list-disc pl-5">
-					{students.map(student => (
-						<li key={student._id} className="mb-2">
-							{student.username} ({student.email})
-						</li>
-					))}
-				</ul>
+		<div
+			className="container mx-auto mb-auto p-4"
+			style={{ maxHeight: "100%" }}
+		>
+			<header className="sticky top-3 bg-gray-100 z-10">
+				<h1 className="text-2xl font-bold mb-6 text-center">
+					Teacher Dashboard
+				</h1>
+				{error && <p className="text-red-500">{error}</p>}
+				<div className="mb-4">
+					<label
+						className="block text-gray-700 mb-2"
+						htmlFor="students"
+					>
+						Filter Students
+					</label>
+					<input
+						type="text"
+						id="students"
+						name="students"
+						value={filter}
+						onChange={e => setFilter(e.target.value)}
+						className="w-full p-2 border border-gray-300 rounded"
+					/>
+				</div>
+			</header>
+			<div className="overflow-x-auto max-h-96">
+				<table className="min-w-full bg-white">
+					<thead className="bg-gray-200 sticky top-0 z-10">
+						<tr>
+							<th className="py-2 px-4 border-b text-start">
+								Username
+							</th>
+							<th className="py-2 px-4 border-b text-start">
+								Email
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						{filteredStudents.map(u => (
+							<tr key={u._id}>
+								<td className="py-2 px-4 border-b">
+									{u.username}
+								</td>
+								<td className="py-2 px-4 border-b">
+									{u.email}
+								</td>
+							</tr>
+						))}
+					</tbody>
+					<tfoot className="bg-gray-100 sticky bottom-0 z-10">
+						<tr>
+							<td className="py-2 px-4 border-t" colSpan="4">
+								{filteredStudents.length} results found
+							</td>
+						</tr>
+					</tfoot>
+				</table>
 			</div>
 		</div>
 	);
